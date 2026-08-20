66e2888a88d687b9c22d1cb038fafac4f59c7eca
hazimfrodo <hazimhanif0@gmail.com>
2026-08-20 12:22:43 +0800

    Navbar logo links to home page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
     >
       <div className="max-w-7xl mx-auto px-6 h-16 flex items-center justify-between">
         {/* Logo */}
-        <a href="#" className="flex items-center gap-3 group">
+        <a href="/" className="flex items-center gap-3 group">
           <Image
             src="/um-logo.webp"
             alt="UM Logo"
